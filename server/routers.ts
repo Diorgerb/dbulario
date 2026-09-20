360cf331c59220b2925deddf890581f6316ab4bc
Diórger Bretas <122491123+Diorgerb@users.noreply.github.com>
2026-09-20 14:59:35 -0300

    feat(bulário): registrar nova rota tRPC mantendo routers existentes

--- server/routers.ts
+++ server/routers.ts
@@ -2,11 +2,13 @@ import { systemRouter } from "./_core/systemRouter.js";
 import { publicProcedure, router } from "./_core/trpc.js";
 import { medicationsRouter } from "./routers/medications.js";
 import { adminRouter } from "./routers/admin.js";
+import { bulaDiffRouter } from "./routers/bulaDiff.js";
 
 export const appRouter = router({
   system: systemRouter,
   medications: medicationsRouter,
   admin: adminRouter,
+  bulaDiff: bulaDiffRouter,
 });
 
 export type AppRouter = typeof appRouter;
